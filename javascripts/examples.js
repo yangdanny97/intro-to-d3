c60bebf7e3682bfcb35dda6264db0b191cfa7c1f
yangdanny97 <dzy4@cornell.edu>
2021-09-05 22:31:01 -0700

    formatting

--- javascripts/examples.js
+++ javascripts/examples.js
@@ -1,10 +1,10 @@
 (() => {
-  /* The examples are executable, let's execute them. */
-  var source = '';
+    /* The examples are executable, let's execute them. */
+    var source = '';
 
-  d3.selectAll('.ex-exec .example-source').each(() => {
-    source += this.innerText;
-  });
+    d3.selectAll('.ex-exec .example-source').each(() => {
+        source += this.innerText;
+    });
 
-  eval(source);
+    eval(source);
 })();
